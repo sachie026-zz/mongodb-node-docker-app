9666c2e35531b57170cbffec0d2d202bd801ae46
Sachin Jadhav <jadhavsachin174@gmail.com>
2020-03-01 11:56:57 +0530

    Add docker/docker-compose/ignore files and create services for db and web

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./src/connections")
+const User = require("./src/User.model");
 
 
 const PORT = 8080;
@@ -9,8 +10,16 @@ app.get('/', (req, res) => {
     res.send("Hello from node server")
 })
 
-app.get('/Users', (req, res) => {
+app.get('/users', async (req, res) => {
     console.log("Get users")
+    const users = await User.find();
+    res.json(users);
+})
+
+app.post('/user-create', async (req, res) => {
+    const user = new User({ username: "userTest" });
+    await user.save().then(() => console.log('User created'))
+    res.send("User created \n");
 })
 
 app.listen(PORT, () => {
